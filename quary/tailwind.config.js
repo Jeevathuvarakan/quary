870a7f7b1f0c257a875966d3d10e11973322b627
Paranitha Anantharajah <paranitha2000@gmail.com>
2025-03-01 13:29:53 +0530

    add screen,server

--- quary/tailwind.config.js
+++ quary/tailwind.config.js
@@ -1,12 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+        mobilelandscape: {
+          raw: "(orientation: landscape) and (max-width: 640px)",
+        },
+        mobileportrait: {
+          raw: "(orientation: portrait) and (max-width: 640px)",
+        },
+      },
+
+      fontFamily: {
+
+        Raleway: ["Raleway", "serif"],
+        Inter: ["Inter", "serif"]
+      },
+    },
   },
   plugins: [],
-}
-
+};
