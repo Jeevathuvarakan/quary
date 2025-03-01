e962894d22a8a417aecf073395b84f8744149bdc
Jeevathuvarakan <tjeevathuvarakan@gmail.com>
2025-03-01 14:27:08 +0530

    import data

---
+++ quary/src/constants/Data.jsx
@@ -0,0 +1,8 @@
+export const HeaderItems = [
+    {label: "Home", path: "/"},
+    {label: "About Us", path: "/aboutus"},
+    {label: "Projects", path: "/projects"},
+    {label: "Services", path: "/services"},
+    {label: "Contact Us", path: "/contactus"},
+];
+
